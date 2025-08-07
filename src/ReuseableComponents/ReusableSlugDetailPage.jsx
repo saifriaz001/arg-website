42e42c427ae3c2557a9836955fb0401d8b00793d
saifriaz001 <89684150+saifriaz001@users.noreply.github.com>
2025-08-07 11:33:26 +0530

    WIP: Save local changes before pull

--- src/ReuseableComponents/ReusableSlugDetailPage.jsx
+++ src/ReuseableComponents/ReusableSlugDetailPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
+import ReactMarkdown from 'react-markdown';
 
 const ReusableSlugDetailPage = ({ data, slugParamName = 'slug', defaultImage, backLink = '/' }) => {
   const params = useParams();
@@ -17,32 +18,46 @@ const ReusableSlugDetailPage = ({ data, slugParamName = 'slug', defaultImage, ba
   }
 
   return (
-    <section className="section-layout ">
-      <div className="max-w-7xl mx-auto">
-        <h1 className="heading-title">{item?.title}</h1>
-        {/* Hero */}
-        <div className="h-80 relative overflow-hidden mb-10">
-          <img
-            src={item.imageUrl || item.image || defaultImage}
-            alt={item.title}
-            className="w-full h-full object-cover brightness-75"
-          />
-
+    <div>
+      <section className="section-layout ">
+        <div className="max-w-7xl mx-auto">
+          <h1 className="heading-title">{item?.title}</h1>
         </div>
-
-        <div  >
-            <h1 className=" slug-heading">{item?.heading}</h1>
-        </div>
-
-        {/* Description */}
-        <div >
-          <p className="slug-Description">
-            {item.description?.replace(/\\n/g, '\n') || 'No description provided.'}
-          </p>
-        </div>
+      </section>
+      <div className="h-80  overflow-hidden mb-10 bg-[#FFFDFA]">
+        <img
+          src={item.imageUrl || item.image || defaultImage}
+          alt={item.title}
+          className="w-full h-full object-cover brightness-100"
+        />
       </div>
-    </section>
+      <section className="section-layout ">
+        <div className="max-w-7xl mx-auto">
+          <div  >
+            <h1 className=" slug-heading">{item?.heading}</h1>
+          </div>
+
+          {/* Description */}
+          <div className='prose prose-lg' >
+            <p className="slug-Description prose prose-lg">
+              
+              {item.description?.replace(/\\n/g, '\n') || 'No description provided.'}
+            </p>
+            <div className='prose prose-lg slug-Description '>
+              <ReactMarkdown>{item.description || 'No description provided.'}</ReactMarkdown>
+            </div>
+            <ul className="list-disc pl-5">
+  {item?.description?.split(/[\.\n]/).map(str => str.trim()).filter(Boolean).map((line, i) => (
+    <li key={i}>{line}</li>
+  ))}
+</ul>
+            
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
 export default ReusableSlugDetailPage;
+ 
